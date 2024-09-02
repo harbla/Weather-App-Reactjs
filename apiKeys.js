1a2eabaa11058bb59d282b4b42ac7c86202a7bb1
SANMENDER <92826995+harbla@users.noreply.github.com>
2024-09-02 18:49:04 +0530

    Add files via upload

---
+++ apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "3224f80047758f8dc355cd5aa5161701",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
